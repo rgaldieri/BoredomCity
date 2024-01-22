f6aabed29eae17ff86b0faea2a6335fb50155324
Multipask17 <pasquale.monniello00@gmail.com>
2024-01-23 00:05:31 +0100

    Added Paint Interaction - DEBUG

--- Assets/Scripts/PaintInteraction.cs
+++ Assets/Scripts/PaintInteraction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,29 +8,38 @@ public class PaintInteraction : MonoBehaviour
 
     bool isInRange;
 
-
     public Canvas interactionCanvas;
-
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if(isInRange){
+            if(Input.GetKeyDown(KeyCode.F)){
+                ShowPaint();
+            }
+        }
     }
 
 
+    private void ShowPaint(){
+        Debug.Log("ENTRA!");
+    }
+
     private void OnTriggerEnter(Collider other){
 
-        if(other.tag == "Player")
-            // isInRange = true;
+        if(other.tag == "Player"){
+            isInRange = true;
             interactionCanvas.gameObject.SetActive(true);
+        }
 
     }
 
     private void OnTriggerExit(Collider other){
 
-        if(other.tag == "Player")
+        if(other.tag == "Player"){
+            isInRange = false;
             interactionCanvas.gameObject.SetActive(false);
+        }
     }
 
 }
